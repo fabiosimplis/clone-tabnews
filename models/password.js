4e09dacb87eec3ffedf7cb0e0746cfc6f18a251a
fabiosimplis <fabio.simplis@gmail.com>
2025-12-27 19:51:21 -0300

    feat: add support for `PATCH` on `/api/v1/users/[username]`

--- models/password.js
+++ models/password.js
@@ -6,11 +6,7 @@ async function hash(password) {
 }
 
 function getNumberOfRounds() {
-  let rounds = 1;
-  if (process.env.NODE_ENV === "production") {
-    rounds = 14;
-  }
-  return rounds;
+  return process.env.NODE_ENV === "production" ? 14 : 1;
 }
 
 async function compare(providePassword, storedPassword) {
