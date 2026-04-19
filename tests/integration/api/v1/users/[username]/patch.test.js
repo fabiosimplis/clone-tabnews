a7ff2c9fa0622258e1f60c4531c18cafe738fe18
fabio.simplis <31253667+fabiosimplis@users.noreply.github.com>
2026-04-19 20:44:35 -0300

    feat: apply `authorization.filterOutput()` to all endpoints

--- tests/integration/api/v1/users/[username]/patch.test.js
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -232,8 +232,6 @@ describe("PATCH  /api/v1/users/[username]", () => {
       expect(responseBody).toEqual({
         id: responseBody.id,
         username: responseBody.username,
-        email: "uniqueEmail2@curso.dev",
-        password: responseBody.password,
         features: ["create:session", "read:session", "update:user"],
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
@@ -334,8 +332,6 @@ describe("PATCH  /api/v1/users/[username]", () => {
       expect(responseBody).toEqual({
         id: defaultUser.id,
         username: "AlteradoPorPrivilegiado",
-        email: defaultUser.email,
-        password: responseBody.password,
         features: defaultUser.features,
         created_at: defaultUser.created_at.toISOString(),
         updated_at: responseBody.updated_at,
